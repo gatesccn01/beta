cc871bed545a2c8ce9619c7776d695c86a55110f
El Mandarin Sniff <105575378+gatesccn01@users.noreply.github.com>
2023-02-20 01:05:45 -0400

    Update install.sh

--- Generador-BOT-main/Instalador/install.sh
+++ Generador-BOT-main/Instalador/install.sh
@@ -44,7 +44,7 @@ tittle() {
     [[ -z $1 ]] && rt='adm-lite' || rt='ADMcgh'
     clear && clear
     msg -bar
-    echo -e "\033[1;44;44m   \033[1;33m=====>>►► 🐲 ChumoGH 💥 Plus 🐲 ◄◄<<=====  \033[0m \033[0;33m[$(less /etc/${rt}/v-local.log)]"
+    echo -e "\033[1;44;44m   \033[1;33m=====>>►► 🐲 BolScript 💥 Plus 🐲 ◄◄<<=====  \033[0m \033[0;33m[$(less /etc/${rt}/v-local.log)]"
     msg -bar
 }
 in_opcion() {
@@ -339,7 +339,7 @@ function_verify() {
         echo -e "\n"
         echo -e "\e[31m    LA IP $(wget -qO- ipv4.icanhazip.com) FUE RECHAZADA!"
         echo -e " $link No AUTORIZADA el ACCESO "
-        echo -e " SI DESEAS USAR EL BOTGEN CONTACTE A @ChumoGH"
+        echo -e " SI DESEAS USAR EL BOTGEN CONTACTE A @gatesccn"
         msg -bar
         [[ -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
         [[ -e /bin/downloadbot ]] && rm -f /bin/downloadbot
